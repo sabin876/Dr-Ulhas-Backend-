86ec7174d2936c3ca840ae8383bc6dc3a0efe254
sabin876 <sabinsabin876@gmail.com>
2026-05-17 18:17:16 +0545

    Fix media and static file serving for production dashboard images

--- core/urls.py
+++ core/urls.py
@@ -29,13 +29,15 @@ sitemaps = {
 
 from api.views import robots_txt
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', robots_txt, name='robots-txt'),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
