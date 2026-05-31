c17c5b1a1560b14d88de35a3720bea193a86dd85
Prajwal <prajwalsiwakoti39@gmail.com>
2026-05-31 08:22:03 +0545

    fix here

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 from decouple import config
 
-
+#fix here
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
